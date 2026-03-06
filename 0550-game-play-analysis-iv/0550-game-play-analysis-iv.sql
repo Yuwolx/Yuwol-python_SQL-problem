4bccbbb21a6efc0e85377f5cf393e6a31dc9b614
Yuwolx <yuwolxx@gmail.com>
2026-03-06 12:55:16 +0900

    Time: 501 ms (94.25%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT ROUND((count(distinct player_id)) / (SELECT COUNT(distinct player_id) FROM activity) ,2) as fraction
+FROM(SELECT player_id, event_date, 
+            RANK() OVER(PARTITION BY player_id ORDER BY event_date) as rnk,
+            DATEDIFF(event_date, LAG(event_date) 
+            OVER (PARTITION BY player_id ORDER BY event_date)) as gaps
+    FROM activity) alpha
+WHERE rnk = 2 AND gaps = 1
+
