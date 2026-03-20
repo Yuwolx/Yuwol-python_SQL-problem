6607c603887618df71917599523d1ae576067e59
Yuwolx <yuwolxx@gmail.com>
2026-03-20 14:09:39 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,14 @@
 # Write your MySQL query statement below
-SELECT person_name
-FROM(SELECT turn, person_name, weight,
-        CASE WHEN turn = 1 then weight
-            WHEN turn != 1 then (SELECT SUM(weight) FROM queue b WHERE b.turn <= a.turn)
-            END AS TOTAL
-    FROM Queue a
-    HAVING total <= 1000
-    ORDER BY turn) alpha
-ORDER BY total DESC
-LIMIT 1
+-- SELECT person_name
+-- FROM(SELECT turn, person_name, weight,
+--         CASE WHEN turn = 1 then weight
+--             WHEN turn != 1 then (SELECT SUM(weight) FROM queue b WHERE b.turn <= a.turn)
+--             END AS TOTAL
+--     FROM Queue a
+--     HAVING total <= 1000
+--     ORDER BY turn) alpha
+-- ORDER BY total DESC
+-- LIMIT 1
+
+SELECT person_name, SUM(weight) OVER(ORDER BY turn) as total_weight
+FROM Queue
