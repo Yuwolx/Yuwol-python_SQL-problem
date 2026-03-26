c71620fa0bfc8aa29366df88bc15cb455dabdb20
Yuwolx <yuwolxx@gmail.com>
2026-03-26 09:10:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,19 +1,18 @@
+WITH base AS 
+(SELECT visited_on, SUM(amount) as amount
+FROM customer
+GROUP BY visited_on)
+
 SELECT visited_on,
-    (SELECT SUM(amount) FROM (SELECT visited_on, SUM(amount) as amount
-                                FROM customer
-                                GROUP BY visited_on) c
+    (SELECT SUM(amount) FROM base
         WHERE visited_on 
             BETWEEN DATE_SUB(a.visited_on, interval 6 day) 
             AND a.visited_on) as amount,
-    (SELECT ROUND(AVG(amount),2) FROM (SELECT visited_on, SUM(amount) as amount
-                                FROM customer
-                                GROUP BY visited_on) c
+    (SELECT ROUND(AVG(amount),2) FROM base
         WHERE visited_on 
             BETWEEN DATE_SUB(a.visited_on, interval 6 day) 
             AND a.visited_on) as average_amount
-FROM (SELECT visited_on, SUM(amount) as amount
-        FROM customer
-        GROUP BY visited_on) a
+FROM base a
 WHERE visited_on >= (SELECT visited_on
                     FROM(SELECT distinct visited_on, 
                                 RANK() OVER(ORDER BY visited_on) as rnk
